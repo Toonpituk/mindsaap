6574ea8fddb43b8d40545c06284ad87f4e02b0d7
Prateep Kul <1.0@kul.com>
2014-04-26 16:49:08 +0700

    Initial Commit
    Initial Commit

---
+++ app/controllers/sample_controller.rb
@@ -0,0 +1,32 @@
+
+# -*- encoding : utf-8 -*-
+class SampleController < ApplicationController
+  def views
+    @title= "Views"
+    @activities = Code.where(:atype=>1).desc(:created_at).page(params[:page]).per(10)
+  end
+  def models
+    @title= "Models"
+    @activities = Code.where(:atype=>2).desc(:created_at).page(params[:page]).per(10)
+    render :template=> "sample/views"
+  end
+  def controllers
+    @title= "Controllers"
+    @activities = Code.where(:atype=>3).desc(:created_at).page(params[:page]).per(10)
+    render :template=> "sample/views"
+  end
+  def show
+    @activity = Code.find params[:id]
+    render :template=> "sample/views"
+  end
+  def rm
+    @activity = Code.find params[:id]
+    @activity.destroy
+    # debugger
+    redirect_to env["HTTP_REFERER"]
+  end
+
+  def create_code
+    Code.create $xvars["entercode"]["code"]
+  end
+end
---
+++ app/views/info/samples.md
@@ -0,0 +1,16 @@
+### Sample View
+## Freemind
+
+![bt9](/assets/sample-freemind.png "Freemind")
+
+This is sample text.
+
+## views
+![bt9](/assets/sample.png "views")
+### This is sample header.
+
+## samples.md
+![bt9](/assets/samplecode.png "sample.md")
+
+## Links to map
+* <%= link_to "Nintaburi Province", "http://www.nonthaburi.go.th/", :target=>"_blank" %>
